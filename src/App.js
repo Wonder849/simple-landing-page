1635de34d88c21accf7d5ce5b57237b18217b51c
vladostrovets <vladostrovets2007@gmail.com>
2025-11-26 17:45:05 +0200

    Branch2ToConflict2.0

--- src/App.js
+++ src/App.js
@@ -13,19 +13,24 @@ function App() {
                 <div className="wrapp">
                     {/* Тут кожен додає свою картку вручну */}
 
+
                     <div className="card">
                         <img className="img" src={oleksandrImg} alt="Oleksandr" />
                         <h2>Krekhovetskyi O.V.</h2>
                         <p>Software Engineer</p>
                     </div>
+
                     <div className="card">
                         <img className="img" src={vladImg} alt="Vlad" />
                         <h2>Ostrovets V.M.</h2>
                         <p>Software Engineer</p>
+
                     </div>
                     <div className="card">
                         <h2>General Card B</h2>
                         <p>This is a general card created in branch B.</p>
+
+
                     </div>
                 </div>
             </main>
